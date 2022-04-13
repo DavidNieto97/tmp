9c0340215b4fde546db2859470e440fb9d58c57f
David Nieto <33969423+DavidNieto97@users.noreply.github.com>
2022-04-12 21:09:52 -0500

    Adding label and history point fields.

--- taskproject/src/main/java/com/web/model/Task.java
+++ taskproject/src/main/java/com/web/model/Task.java
@@ -1,5 +1,6 @@
 package com.web.model;
 
+import java.awt.*;
 import java.time.LocalDate;
 
 public class Task {
@@ -10,4 +11,6 @@ public class Task {
     private long assignedTo;
     private TaskStatus status;
     private String comments;
+    private String label;
+    private String historyPoint;
 }
