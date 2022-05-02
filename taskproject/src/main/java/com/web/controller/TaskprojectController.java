8ad60255516c69e148d144fcfc9857b34fd8b9b2
David Nieto <33969423+DavidNieto97@users.noreply.github.com>
2022-05-02 16:54:39 -0500

    Deleting unused files.

---
+++ taskproject/src/main/java/com/web/controller/TaskprojectController.java
@@ -0,0 +1,19 @@
+package com.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class TaskprojectController
+{
+    @GetMapping("/main")
+    public String main(Model model)
+    {
+        return "main";
+    }
+}
+
+
+
